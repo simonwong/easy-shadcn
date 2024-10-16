5db1eb5541e1df56eda40950ce3a8809d3d004f2
Simon <wsj_simon@163.com>
2024-10-16 14:51:37 +0800

    chore: add eslint&prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslintConfig from '@yueqing/eslint-config-ts';
+import tailwindcssConfig from '@yueqing/eslint-config-ts/tailwindcss-config';
+
+export default eslintConfig(
+  {
+    project: ['./tsconfig.eslint.json'],
+    rootDir: import.meta.dirname,
+  },
+  [
+    ...tailwindcssConfig,
+    {
+      ignores: [
+        '.turbo/',
+        '.next/',
+        'eslint.config.mjs',
+        'packages/react/components/',
+        'storybook-static/',
+      ],
+    },
+  ]
+);
