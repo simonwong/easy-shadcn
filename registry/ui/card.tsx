173600950e023bb0ae33a5f3ba41aacf643ceb7b
Simon <wsj_simon@163.com>
2025-07-28 17:53:26 +0800

    feat(card): 新增 Card 组件的 action、headerClassName、titleClassName、descriptionClassName 和 contentClassName 属性，优化示例以展示新特性

--- registry/ui/card.tsx
+++ registry/ui/card.tsx
@@ -1,6 +1,7 @@
 import type React from 'react';
 import type { ReactNode } from 'react';
 import {
+  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
@@ -11,49 +12,91 @@ import {
 import { cn } from '@/lib/utils';
 
 export interface CardProps
-  extends Omit<React.ComponentProps<typeof InternalCard>, 'title' | 'content'> {
+  extends Omit<React.ComponentProps<typeof InternalCard>, 'title'> {
+  headerClassName?: string;
   headerProps?: React.ComponentProps<typeof CardHeader>;
   title?: ReactNode;
+  titleClassName?: string;
   titleProps?: React.ComponentProps<typeof CardTitle>;
   description?: ReactNode;
+  descriptionClassName?: string;
   descriptionProps?: React.ComponentProps<typeof CardDescription>;
-  content?: ReactNode;
+  action?: ReactNode;
+  actionClassName?: string;
+  actionProps?: React.ComponentProps<typeof CardAction>;
+  contentClassName?: string;
   contentProps?: React.ComponentProps<typeof CardContent>;
   footer?: ReactNode;
+  footerClassName?: string;
   footerProps?: React.ComponentProps<typeof CardFooter>;
 }
 
 export const Card: React.FC<CardProps> = ({
+  headerClassName,
   headerProps,
   title,
+  titleClassName,
   titleProps,
   description,
+  descriptionClassName,
   descriptionProps,
-  content,
+  action,
+  actionClassName,
+  actionProps,
+  contentClassName,
   contentProps,
   footer,
+  footerClassName,
   footerProps,
+  children,
   ...resetProps
 }) => {
   return (
     <InternalCard {...resetProps} className={cn(resetProps?.className)}>
       {(title || description) && (
-        <CardHeader {...headerProps} className={cn(headerProps?.className)}>
-          {title && <CardTitle {...titleProps}>{title}</CardTitle>}
+        <CardHeader
+          {...headerProps}
+          className={cn(headerProps?.className, headerClassName)}
+        >
+          {title && (
+            <CardTitle
+              {...titleProps}
+              className={cn(titleProps?.className, titleClassName)}
+            >
+              {title}
+            </CardTitle>
+          )}
           {description && (
-            <CardDescription {...descriptionProps}>
+            <CardDescription
+              {...descriptionProps}
+              className={cn(descriptionProps?.className, descriptionClassName)}
+            >
               {description}
             </CardDescription>
           )}
+          {action && (
+            <CardAction
+              {...actionProps}
+              className={cn(actionProps?.className, actionClassName)}
+            >
+              {action}
+            </CardAction>
+          )}
         </CardHeader>
       )}
-      {content && (
-        <CardContent {...contentProps} className={cn(contentProps?.className)}>
-          {content}
+      {children && (
+        <CardContent
+          {...contentProps}
+          className={cn(contentProps?.className, contentClassName)}
+        >
+          {children}
         </CardContent>
       )}
       {footer && (
-        <CardFooter {...footerProps} className={cn(footerProps?.className)}>
+        <CardFooter
+          {...footerProps}
+          className={cn(footerProps?.className, footerClassName)}
+        >
           {footer}
         </CardFooter>
       )}
