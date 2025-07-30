4c55f00e3190f975b273315ddf4e70bf4fa2ca31
Simon <wsj_simon@163.com>
2025-07-30 16:45:38 +0800

    feat: add tabs

--- registry/ui/tabs.tsx
+++ registry/ui/tabs.tsx
@@ -3,57 +3,85 @@
 import type {
   TabsProps as InternalTabsProps,
   TabsContentProps,
-  TabsListProps,
-  TabsTriggerProps,
 } from '@radix-ui/react-tabs';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+import type { ClassValue } from 'class-variance-authority/types';
 import React, { type ReactNode } from 'react';
-import {
-  Tabs as InternalTabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '@/components/ui/tabs';
+import { cn } from '@/lib/utils';
 
 export interface TabsProps extends InternalTabsProps {
   option: {
     value: string;
     title: ReactNode;
-    triggerProps?: Omit<TabsTriggerProps, 'value'>;
+    triggerClassName?: ClassValue;
     content: ReactNode;
-    contentProps?: Omit<TabsContentProps, 'value'>;
+    contentClassName?: ClassValue;
+    forceMount?: true;
   }[];
-  listProps?: TabsListProps;
-  triggerProps?: Omit<TabsTriggerProps, 'value'>;
+  tabBarLoop?: boolean;
+  tabBarClassName?: ClassValue;
+  triggerClassName?: ClassValue;
   contentProps?: Omit<TabsContentProps, 'value'>;
 }
 
 export const Tabs = React.forwardRef<HTMLDivElement, TabsProps>(
-  ({ option, listProps, triggerProps, contentProps, ...props }, ref) => {
+  (
+    {
+      option,
+      tabBarLoop,
+      tabBarClassName,
+      triggerClassName,
+      contentProps,
+      className,
+      ...props
+    },
+    ref
+  ) => {
     return (
-      <InternalTabs ref={ref} {...props}>
-        <TabsList {...listProps}>
+      <TabsPrimitive.Root
+        className={cn('flex flex-col gap-2', className)}
+        ref={ref}
+        {...props}
+      >
+        <TabsPrimitive.List
+          className={cn(
+            'inline-flex h-9 w-fit items-center justify-center rounded-lg bg-muted p-[3px] text-muted-foreground',
+            tabBarClassName
+          )}
+          data-slot="tabs-list"
+          loop={tabBarLoop}
+        >
           {option.map((opt) => (
-            <TabsTrigger
-              {...triggerProps}
-              {...opt.triggerProps}
+            <TabsPrimitive.Trigger
+              className={cn(
+                "inline-flex h-[calc(100%-1px)] flex-1 cursor-pointer items-center justify-center gap-1.5 whitespace-nowrap rounded-md border border-transparent px-2 py-1 font-medium text-foreground text-sm transition-[color,box-shadow] focus-visible:border-ring focus-visible:outline-1 focus-visible:outline-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:shadow-sm dark:text-muted-foreground dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 dark:data-[state=active]:text-foreground [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+                triggerClassName,
+                opt.triggerClassName
+              )}
+              data-slot="tabs-trigger"
               key={opt.value}
               value={opt.value}
             >
               {opt.title}
-            </TabsTrigger>
+            </TabsPrimitive.Trigger>
           ))}
-        </TabsList>
+        </TabsPrimitive.List>
         {option.map((opt) => (
-          <TabsContent
-            {...contentProps}
-            {...opt.contentProps}
+          <TabsPrimitive.Content
+            className={cn(
+              'flex-1 outline-none',
+              opt.contentClassName,
+              className
+            )}
+            data-slot="tabs-content"
+            forceMount={opt.forceMount}
             key={opt.value}
             value={opt.value}
           >
             {opt.content}
-          </TabsContent>
+          </TabsPrimitive.Content>
         ))}
-      </InternalTabs>
+      </TabsPrimitive.Root>
     );
   }
 );
