0f6b2106d38c565b9170ede9c06862800a88ac34
Simon <wsj_simon@163.com>
2025-12-23 23:01:20 +0800

    feat: update example

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -2,8 +2,6 @@
 
 import * as CommandModal from "@easy-shadcn/command-modal";
 import { Button } from "@/registry/ui/button";
-import { Card } from "@/registry/ui/card";
-import { Tabs } from "@/registry/ui/tabs";
 import { FormModal } from "./features/FormModal";
 import { InfoModal } from "./features/InfoModal";
 import { PreviewGroup } from "./features/PreviewGroup";
@@ -11,39 +9,6 @@ import { PreviewGroup } from "./features/PreviewGroup";
 const CompositePreview = () => {
   return (
     <div className="flex flex-wrap">
-      <PreviewGroup>
-        <Tabs
-          className="max-w-sm"
-          defaultValue="login"
-          option={[
-            {
-              title: "Login",
-              value: "login",
-              content: (
-                <Card
-                  action={<div>Extra</div>}
-                  className="w-80"
-                  description="Card Description"
-                  footer={<Button>Hi</Button>}
-                  footerClassName="flex justify-end"
-                  title="Login"
-                >
-                  Card Content, Card Content, Card Content, Card Content
-                </Card>
-              ),
-            },
-            {
-              title: "Signup",
-              value: "signup",
-              content: (
-                <Card title="Signup Card">
-                  Card Content, Card Content, Card Content, Card Content
-                </Card>
-              ),
-            },
-          ]}
-        />
-      </PreviewGroup>
       <PreviewGroup>
         <Button
           onClick={() => {
